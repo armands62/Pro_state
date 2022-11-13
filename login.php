236ca8d685bae242e47c13f9f5a132e5dae80746
Gundars Rēbuks <rebuks.gundars@gmail.com>
2022-11-13 13:38:05 +0200

    Login/register system backend

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+if(!empty($_SESSION["logged"])) {
+    header("Location: index.php");
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+    <link href="css/main.css" type="text/css" rel="stylesheet">
+    <title>Pro State Bank</title>
+</head>
+<body>
+<main class="login-main">
+    <div class="login-container">
+        <h2>Ielogoties</h2>
+        <?php
+        if(!empty($_SESSION["login_err_msg"])){
+            echo "<p class='login-error'>{$_SESSION["login_err_msg"]}</p>";
+            unset($_SESSION["login_err_msg"]);
+        }
+        ?>
+        <form action="authenticate.php" method="POST">
+            <label for="email">E-pasta adrese</label>
+            <input type="text" name="email" placeholder="E-pasts" id="email" required>
+
+            <label for="password">Parole</label>
+            <input type="password" name="password" placeholder="Parole" id="password" required>
+            <input type="submit" value="Log in" id="submit">
+        </form>
+        <p><a href="signup.php">Vai jūs vēl neesat reģistrējies? Reģistrējieties šeit!</a></p>
+        <p><a href="index.php">Atgriezties galvenajā lapā</a></p>
+    </div>
+</main>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+if(!empty($_SESSION["logged"])) {
+    header("Location: index.php");
+}
+?>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/main.css" type="text/css" rel="stylesheet">
+    <title>Pro State Bank</title>
+</head>
+<body>
+<main class="login-main">
+    <div class="login-container">
+        <h2>Reģistrēties</h2>
+        <?php
+        if(!empty($_SESSION["login_err_msg"])){
+            echo "<p class='login-error'>{$_SESSION["login_err_msg"]}</p>";
+            unset($_SESSION["login_err_msg"]);
+        }
+        ?>
+        <form action="register.php" method="POST">
+            <label for="name">Vārds</label>
+            <input type="text" name="name" placeholder="Vārds" id="name" required>
+
+            <label for="surname">Uzvārds</label>
+            <input type="text" name="surname" placeholder="Uzvārds" id="surname" required>
+
+            <label for="birthday">Dzimšanas datums</label>
+            <input type="date" name="birthday" id="birthday" required>
+
+            <label for="gender">Dzimums</label>
+            <select id="gender" name="gender" required>
+                <option value="male">Vīrietis</option>
+                <option value="female">Sieviete</option>
+                <option value="other">Nezinu</option>
+            </select>
+
+            <label for="email">E-pasta adrese</label>
+            <input type="email" name="email" placeholder="E-pasts" id="email" required>
+
+            <label for="password">Parole</label>
+            <input type="password" name="password" placeholder="Parole" id="password" required>
+
+            <input type="submit" value="Reģistrēties" id="submit">
+        </form>
+        <p><a href="login.php">Vai konts jau eksistē? Pierakstieties šeit!</a></p>
+        <p><a href="index.php">Atgriezties galvenajā lapā</a></p>
+    </div>
+</main>
+</body>
+</html>
