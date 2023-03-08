bb1e55a71468ccf7f6b6ce0d8ab92f4d5acb500c
Gundars Rēbuks <rebuks.gundars@gmail.com>
2023-03-08 12:55:22 +0200

    Fixed admin message response

--- src/admin/answer_request.php
+++ src/admin/answer_request.php
@@ -6,7 +6,6 @@ include_once("./backend/userinfo.php");
 <main class="admin-answer-request-main">
     <h1 class="answer-request-h1">Atbildēt uz lietotāja ziņu</h1>
         <div class="answer-content">
-        <form>
         <?php
         $request = UserInfo::get_request($_GET['id']);
         $profile = UserInfo::get_profile($request['user_id']);
@@ -16,15 +15,15 @@ include_once("./backend/userinfo.php");
         echo "<p>{$request['date']}</p>";
         echo "<p>{$request['description']}</p>";
         echo "</div>";
-        echo "<form action='/send_answer_request?id={$_GET['id']}' method='POST'>"
+        echo "<form action='/send_answer_request?id={$_GET['id']}' method='POST'>";
         ?>
-        </form>
-        </div>
-        <div class="answer-log">
-            <!--<label for="answer">Atbilde</label>-->
-            <textarea id="answer" name="answer" rows="12" cols="100"></textarea>
+            <div class="answer-log">
+                <!--<label for="answer">Atbilde</label>-->
+                <textarea id="answer" name="answer" rows="12" cols="100"></textarea>
 
-            <input type="submit" value="Sūtīt atbildi" class="submit-answer">
+                <input type="submit" value="Sūtīt atbildi" class="submit-answer">
+            </div>
+        </form>
         </div>
 </main>
 <?php
